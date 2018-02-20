a5f13462a00d58ede16b4810e821232aaf7b697d
michaelmegliola <michael.megliola@gmail.com>
2018-02-20 13:17:26 -0500

    Initial commit

---
+++ linear_regression.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+
+class MyGraph:
+    def __init__(self, x, y):
+        plt.ion()
+        self.figure = plt.figure()
+        self.ax = self.figure.add_subplot(111)
+        self.line1, = self.ax.plot(x, y, 'o')
+        self.line2, = self.ax.plot(x, y, 'b-')
+        self.count = 0
+
+    def update(self, m, b, error):
+        plt.title('{:0d}'.format(self.count) + ' m=' + '{:.3f}'.format(m) + ' b=' + '{:.3f}'.format(b) + ' error=' + '{:.3f}'.format(error))
+        self.line1.set_ydata(y)
+        yi = [m*xi+b for xi in x]
+        self.line2.set_ydata(yi)
+        self.figure.canvas.draw()
+        self.count += 1
+
+
+x = [0, 1, 2, 3, 4, 5]
+y = [0, 2, 1, 7, 4, 12]
+
+graph = MyGraph(x, y)
+
+m = np.random.rand() * 5 - 10
+b = np.random.rand() * 5 - 10
+
+alpha = 0.001
+prior_error = 0
+
+for i in range(10000):
+
+    # these values are computed as the sums of series, so start at zero
+    error = 0
+    derror_dm = 0
+    derror_db = 0
+
+    for j in range(len(x)):
+        # ------------------------------------------------------------------
+        #
+        #        error = (prediction - observation)^2
+        #
+        #       ( outer func is diff squared  )
+        #                 |=prediction=| |obsv|
+        error += np.square(m * x[j] + b - y[j])
+
+        #           |=derivative of outer fn=|  | w/r/t m|
+        derror_dm += 2 * (m * x[j] + b - y[j]) * x[j]
+
+        #           |=derivative of outer fn=|  | w/r/t b|
+        derror_db += 2 * (m * x[j] + b - y[j]) * 1
+
+    # take a step in the right direction...
+    m -= derror_dm * alpha  # the direction is de/dm
+    b -= derror_db * alpha  # the direction is de/db
+
+    graph.update(m, b, error)
+
+    d_error = error - prior_error
+    prior_error = error
+
+    if abs(d_error) < 0.000001:
+        time.sleep(120)
+        break
---
+++ polynomial_regression.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+
+class MyGraph:
+    def __init__(self, x, y):
+        plt.ion()
+        self.count = 0
+        self.figure = plt.figure()
+        self.ax = self.figure.add_subplot(111)
+        self.line1, = self.ax.plot(x, y, 'o')
+        self.line2, = self.ax.plot(x, y, 'b-')
+        self.x_curve = np.arange(-10, 10, 0.1)
+
+    def update(self, a, b, c, error):
+        plt.title('{:0d}'.format(self.count) + ' a=' + '{:.3f}'.format(a) + ' b=' + '{:.3f}'.format(b) + ' c=' + '{:.3f}'.format(
+            c) + ' error=' + '{:.3f}'.format(error))
+        self.line1.set_ydata(y)
+        yi = [a * (xi ** 2) + b * xi + c for xi in self.x_curve]
+        self.line2.set_xdata(self.x_curve)
+        self.line2.set_ydata(yi)
+        self.figure.canvas.draw()
+        self.count += 1
+
+
+x = [0, 1, 2, 3, 4, 5]
+y = [0, 2, 1, 7, 4, 12]
+
+graph = MyGraph(x, y)
+
+a = np.random.rand() * 5 - 10
+b = np.random.rand() * 5 - 10
+c = np.random.rand() * 5 - 10
+
+alpha = 0.00025
+prior_error = 0
+
+for i in range(10000):
+
+    # these values are computed as the sums of series, so start at zero
+    error = 0
+    derror_da = 0
+    derror_db = 0
+    derror_dc = 0
+
+    for j in range(len(x)):
+        # ------------------------------------------------------------------
+        #
+        #        error = (prediction - observation)^2
+        #
+        #       (--- outer function is difference squared ---)
+        #                 |========prediction========| |obsv|
+        error += np.square(a * x[j] ** 2 + b * x[j] + c - y[j])
+
+        #           |====derivative of outer function =====|   | w/r/t a |
+        derror_da += 2 * (a * x[j] ** 2 + b * x[j] + c - y[j]) * (x[j] ** 2)
+
+        #           |====derivative of outer function =====|   | w/r/t b |
+        derror_db += 2 * (a * x[j] ** 2 + b * x[j] + c - y[j]) * x[j]
+
+        #           |====derivative of outer function =====|   | w/r/t c |
+        derror_dc += 2 * (a * x[j] ** 2 + b * x[j] + c - y[j]) * 1
+        # ------------------------------------------------------------------
+
+    # take a step in the right direction...
+    a -= derror_da * alpha  # ...the direction is de/da
+    b -= derror_db * alpha  # ...the direction is de/db
+    c -= derror_dc * alpha  # ...the direction is de/dc
+
+    graph.update(a, b, c, error)
+
+    d_error = error - prior_error
+    prior_error = error
+
+    if abs(d_error) < 0.000001:
+        time.sleep(120)
+        break
